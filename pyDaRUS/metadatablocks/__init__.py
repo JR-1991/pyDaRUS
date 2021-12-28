c16f5a416850b67bb949a8af8625bdacf5a168a4
JR-1991 <30547301+JR-1991@users.noreply.github.com>
2021-12-28 13:14:35 +0100

    Minor changes

--- pyDaRUS/metadatablocks/__init__.py
+++ pyDaRUS/metadatablocks/__init__.py
@@ -1,8 +1,8 @@
 
-from .citation import Citation
-from .enzymeML import EnzymeMl
-from .engMeta import EngMeta
-from .codeMeta import CodeMeta
-from .archive import Archive
-from .process import Process
-from .privacy import Privacy
+from pyDaRUS.metadatablocks.citation import Citation
+from pyDaRUS.metadatablocks.enzymeML import EnzymeMl
+from pyDaRUS.metadatablocks.engMeta import EngMeta
+from pyDaRUS.metadatablocks.codeMeta import CodeMeta
+from pyDaRUS.metadatablocks.archive import Archive
+from pyDaRUS.metadatablocks.process import Process
+from pyDaRUS.metadatablocks.privacy import Privacy
