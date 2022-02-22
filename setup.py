a34c5041f2095a0a82baca33d43684c6dade70c6
JR-1991 <30547301+JR-1991@users.noreply.github.com>
2022-02-22 16:49:55 +0100

    Fixed standard collections as types

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyDaRUS',
-    version='1.0.3',
+    version='1.0.4',
     author='easyDataverse',
     license='MIT License',
     packages=setuptools.find_packages(),
