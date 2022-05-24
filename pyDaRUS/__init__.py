99e8db261cdc8216c39431d050f01fe3c9c74588
JR-1991 <range.jan@web.de>
2022-05-24 11:28:59 +0200

    Added more standard Dataverse metadatablocks

--- pyDaRUS/__init__.py
+++ pyDaRUS/__init__.py
@@ -1,10 +1,15 @@
 import os
 
 from easyDataverse import Dataset
+from pyDaRUS.metadatablocks import Astrophysics
 from pyDaRUS.metadatablocks import Citation
 from pyDaRUS.metadatablocks import EnzymeMl
+from pyDaRUS.metadatablocks import Journal
 from pyDaRUS.metadatablocks import EngMeta
+from pyDaRUS.metadatablocks import Socialscience
 from pyDaRUS.metadatablocks import CodeMeta
+from pyDaRUS.metadatablocks import Biomedical
+from pyDaRUS.metadatablocks import Geospatial
 from pyDaRUS.metadatablocks import Archive
 from pyDaRUS.metadatablocks import Process
 from pyDaRUS.metadatablocks import Privacy
--- pyDaRUS/metadatablocks/__init__.py
+++ pyDaRUS/metadatablocks/__init__.py
@@ -1,8 +1,13 @@
 
+from pyDaRUS.metadatablocks.astrophysics import Astrophysics
 from pyDaRUS.metadatablocks.citation import Citation
 from pyDaRUS.metadatablocks.enzymeML import EnzymeMl
+from pyDaRUS.metadatablocks.journal import Journal
 from pyDaRUS.metadatablocks.engMeta import EngMeta
+from pyDaRUS.metadatablocks.socialscience import Socialscience
 from pyDaRUS.metadatablocks.codeMeta import CodeMeta
+from pyDaRUS.metadatablocks.biomedical import Biomedical
+from pyDaRUS.metadatablocks.geospatial import Geospatial
 from pyDaRUS.metadatablocks.archive import Archive
 from pyDaRUS.metadatablocks.process import Process
 from pyDaRUS.metadatablocks.privacy import Privacy
