ae9c06288c8f9aec3ab073ecb04a6b56e7a46bb8
JR-1991 <range.jan@web.de>
2022-05-24 11:54:11 +0200

    Reverted changes

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
 import setuptools
 from setuptools import setup
 
-with open("README.rst") as f:
-    readme = f.read()
-
 setup(
-    name='pyDaRUS',
-    version='1.0.5',
-    author='easyDataverse',
-    license='MIT License',
+    name="pyDaRUS",
+    version="1.0.5",
+    author="easyDataverse",
+    license="MIT License",
     packages=setuptools.find_packages(),
-    long_description=readme,
     install_requires=[
-        'easyDataverse',
-        'fastapi',
-        'uvicorn',
-        'pydantic',
-        'jinja2',
-        'pyDataverse',
-        'pandas',
-        'pyaml',
-    ]
+        "easyDataverse",
+        "fastapi",
+        "uvicorn",
+        "pydantic",
+        "jinja2",
+        "pyDataverse",
+        "pandas",
+        "pyaml",
+    ],
 )
