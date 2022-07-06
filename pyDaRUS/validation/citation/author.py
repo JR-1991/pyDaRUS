b36b7dff5bb651b7d5add2b96ff6b9563d4965d4
JR-1991 <range.jan@web.de>
2022-07-06 15:51:54 +0200

    Updated author validation

--- pyDaRUS/validation/citation/author.py
+++ pyDaRUS/validation/citation/author.py
@@ -14,18 +14,29 @@ def validate_author(author: Author, allow_edit: bool = False):
         allow_edit (bool, optional): Whether or not proposed edits should be applied. Defaults to False.
     """
 
+    # Initialize report
+    report = {}
+
+    if not author.name:
+        return False, {"loc": "Author", "message": "Author missing name"}
+
     # Get the identifier scheme
     if author.identifier_scheme:
+        # Validate identifier
         identifier_scheme = IdentifierScheme(author.identifier_scheme)
         scheme_valid_fun = scheme_factory(identifier_scheme)
 
-        author_valid, report = scheme_valid_fun(author)
+        author_valid, sub_report = scheme_valid_fun(author)
+
+        report["name"] = sub_report
 
         return author_valid, report
 
     else:
-        # Search for an author on ORCID
-        return "None", {}
+        # Search author via ORCID
+        author_valid, report = _retrieve_by_name_from_orcid(author.name)
+
+        return author_valid, report
 
 
 def scheme_factory(scheme: IdentifierScheme) -> Callable:
@@ -69,9 +80,8 @@ def _check_names(author: Author, orcid_entry: Dict):
     """Checks if the names from an author match to the ORCID entry"""
 
     # Split both names and create a set of words
-    orcid_names = set(
-        orcid_entry["given-names"].split(" ") + orcid_entry["family-names"].split(" ")
-    )
+    orcid_name = " ".join([orcid_entry["given-names"], orcid_entry["family-names"]])
+    orcid_names = set(orcid_name.split(" "))
 
     ds_name = set(author.name.split(" "))
 
@@ -80,11 +90,8 @@ def _check_names(author: Author, orcid_entry: Dict):
     if diff:
         is_valid = False
         report = {
-            "loc": f"Author/{author.name}",
-            "message": f"Author name for ORCID entry {author.identifier} {author.name} matches expect for the following name/s: {diff}",
-            "proposed-change": " ".join(
-                [orcid_entry["given-names"], orcid_entry["family-names"]]
-            ),
+            "message": f"Author name for ORCID entry ('{author.identifier}', '{orcid_name}') matches with Dataverse ('{author.name}') expect for the following name/s: {diff}",
+            "proposed-changes": {"name": orcid_name},
         }
 
     else:
@@ -94,15 +101,52 @@ def _check_names(author: Author, orcid_entry: Dict):
     return is_valid, report
 
 
-if __name__ == "__main__":
-    author = Author(
-        name="Jan Range",
-        identifier_scheme=IdentifierScheme.orcid,
-        identifier="0000-0001-6478-1051",
-        affiliation="SimTech",
-    )
+def _retrieve_by_name_from_orcid(name: str):
+    """Retrieves an ORCID ID from a givebn name.
 
-    author_valid, report = validate_author(author)
+    Args:
+        name (str): Author name of teh Dataverse dataset
+    """
 
-    print(author_valid)
-    print(json.dumps(report, indent=2))
+    def score_name(target: str, query: str):
+        """Matches a name and returns a percent identity"""
+
+        target_set = set(target.split(" "))
+        query_set = set(query.split(" "))
+
+        diff = sum([1 for name in query_set if name in target_set])
+
+        return diff / len(target_set)
+
+    # Set up and perform HTTP request
+    url = f"https://pub.orcid.org/v3.0/expanded-search/?q={name}"
+    response = requests.get(url, headers={"Accept": "application/vnd.orcid+json"})
+    results = response.json()["expanded-result"]
+
+    # Score all occuring result entries
+    matches = []
+    for result in results:
+        orcid_name = " ".join([str(result["given-names"]), str(result["family-names"])])
+        orcid_id = result["orcid-id"]
+
+        score = score_name(name, orcid_name)
+        matches.append((orcid_name, orcid_id, score))
+
+    # Sort everything and add all top entries with same score
+    matches = sorted(matches, key=lambda x: x[-1], reverse=True)
+    best_score = matches[0][-1]
+
+    proposals = [
+        {
+            "name": name,
+            "identifier": orcid_id,
+            "identifier_scheme": IdentifierScheme.orcid.value,
+        }
+        for name, orcid_id, score in matches
+        if score == best_score
+    ]
+
+    return False, {
+        "message": f"Author '{name}' is missing an identifier. The name has been used to retrieve from ORCID and is attached in 'porposed-changes'.",
+        "proposed-changes": proposals,
+    }
